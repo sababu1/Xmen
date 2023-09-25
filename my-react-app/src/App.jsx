017046ec6766803a67599d3d20782cbd561fd0b8
COR Coding <corcoding@CORs-Air.localdomain>
2023-09-25 09:02:07 -0400

    done

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import React from 'react'
+import './App.css'
+import Movies from './components/Movies.jsx'; // Import the Movies component
+function App() {
+ 
+
+  return (
+    <>
+    <Movies/>
+    </>
+  )
+}
+
+export default App
---
+++ my-react-app/src/components/Movies.jsx
@@ -0,0 +1,80 @@
+
+import React, { useState, useEffect } from 'react';
+
+
+const Movies = () => {
+  const [movies, setMovies] = useState([]); //holds titles
+  const [filteredMovie, setFilteredMovie] = useState(null); //holds details
+
+  useEffect(() => {
+   
+    const fetchXMenMovies = async () => {  //Similar to a GET request
+      const apiKey = 'f22241e5';
+      const response = await fetch(
+        `http://www.omdbapi.com/?s=X-men&apikey=${apiKey}`
+      );
+
+      if (response.ok) { //checks for successful response
+        const data = await response.json();
+        if (data.Search) {
+          setMovies(data.Search);  //checks for array with search
+        } 
+      }
+    };
+
+    fetchXMenMovies();
+  }, []);  //Memorize this notation
+
+  const fetchMovieDetails = async (imdbID) => { //finds details using imdbID number
+    const apiKey = 'f22241e5'; 
+    const response = await fetch(
+      `http://www.omdbapi.com/?i=${imdbID}&apikey=${apiKey}`
+    );
+
+    if (response.ok) {  //On success adds data to 'filtered movie' state
+      const data = await response.json();
+      setFilteredMovie(data);
+    } 
+  };
+
+  const closeDetails = () => {    //This is called when the x-button is clicked
+    setFilteredMovie(null);       //resets page
+  };
+
+  return (
+    <div class = "title">
+      <h1>X-Men Movies</h1>
+
+      <div class="movie-list">
+        {movies.map((movie) => (
+          <div key={movie.imdbID} class="movie-item">
+            <img src={movie.Poster} alt={'Xmen'} />
+            <h3>{movie.Title}</h3>
+            <button onClick={() => fetchMovieDetails(movie.imdbID)}>
+              Show Details
+            </button>
+          </div>
+        ))}
+      </div>
+
+      {filteredMovie && (  //conditional rendering for SHowDetails button when fetchMovieDetails function is called
+        <div class="topWindow">
+          <div class="filtered-movie">
+            <button onClick={closeDetails} class="x-button">
+              Close
+            </button>
+            <h2>{filteredMovie.Title}</h2>
+            <p>Year: {filteredMovie.Year}</p>
+            <p>Director: {filteredMovie.Director}</p>
+            <p>IMDb Rating: {filteredMovie.imdbRating}</p>
+            <p>Plot: {filteredMovie.Plot}</p>
+            <img src={filteredMovie.Poster} alt={'XmenMovies'} />
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Movies;
+
